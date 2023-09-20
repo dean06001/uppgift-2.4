57fa59300a061e0ed1df7083d7b39154435e8d8e
dean06001 <dean06001@E-PW0234KC>
2023-09-20 10:52:34 +0200

    Add project files.

---
+++ uppgift 2.4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace uppgift_2_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Det här är lön1");
+            double lön1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Det här är lön2");
+            double lön2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Det här är lön3");
+            double lön3 = double.Parse(Console.ReadLine());
+
+            double medelvärde = (lön1 + lön2 + lön3) / 3;
+            Console.WriteLine("medellönen av lönerna är" + medelvärde);
+            Console.ReadKey();
+           
+        }
+    }
+}
